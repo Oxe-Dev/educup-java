26f619edc9e98b94de68033ac944cb24449a7035
OxerDev <63127095+Oxe-Dev@users.noreply.github.com>
2021-04-12 20:50:21 -0300

    Classe da primeira atividade

---
+++ Atividade01/Marca.java
@@ -0,0 +1,16 @@
+package Atividade01;
+
+public class Marca {
+    
+    private String name;
+
+    //4 
+    public String getName() {
+        return name;
+    }
+    //5
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
