ce007ef1217c80103f6a3524dc96667c533645a7
OxerDev <63127095+Oxe-Dev@users.noreply.github.com>
2021-04-12 20:46:13 -0300

    Arquivo principaç

---
+++ App.java
@@ -0,0 +1,11 @@
+import Atividade01.Marca;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Marca marca = new Marca(); //2 e 3
+        marca.setName("Oxê");//5
+        System.out.println("Marca é: "+marca.getName()); //6
+
+    }
+}
